b1b38c7d2324971d5f4bfb9a7e096bb77ae0d39b
Alexandr <headofthefather@gmail.com>
2023-01-10 17:09:10 +0300

    7_HomeWork is done

---
+++ 7_HomeWork/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. Тип данных для элементов выбрать double, наличие ненулевой дробной части обязательно.
+
+m = 3, n = 4.
+0.5 7.96 -2.78 -0.2
+1.7 -3.3 8.574 -9.9
+8.5 7.87 -7.1 9.15
+*/
+
+void FillArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+        for (int j = 0; j < array.GetLength(1); j++)
+		{
+			array[i, j] = new Random().Next(-1000,1000) / 100.0;
+		}
+
+    }
+}
+
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		Console.Write($"[");
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			
+			Console.Write($"{array[i, j]} ");
+			
+		}
+	    Console.Write($"]");
+	    Console.WriteLine();
+	}
+}
+
+Console.WriteLine("Enter the number of rows");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] array =  new double[m,n];
+FillArray(array);
+Console.WriteLine("This is your double array");
+PrintArray(array);
---
+++ 7_HomeWork/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Напишите программу, которая на вход принимает позиции (два индекса) элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+[1,7] -> такого числа в массиве нет
+*/
+
+void FillArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+        for (int j = 0; j < array.GetLength(1); j++)
+		{
+			array[i, j] = new Random().Next(1,101);
+		}
+
+    }
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+        Console.Write($"[");
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write($"{array[i, j]} ");
+		}
+        Console.Write($"]");
+	    Console.WriteLine();
+	}
+}
+
+Random randomArraySize = new Random();
+int[,] randomArray = new int[randomArraySize.Next(3, 11),randomArraySize.Next(3, 11)];
+FillArray(randomArray);
+PrintArray(randomArray);
+Console.WriteLine("Enter the number of row");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns");
+int m = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    Console.WriteLine($"Element [{n},{m}] is: {randomArray[n,m]}");
+}
+catch(Exception)
+{
+    Console.WriteLine("This element is not exist!");
+}
